985f8ea46f5a34243ef0c6be760ef60d29e434b9
Agera-One <182347485+Agera-One@users.noreply.github.com>
2025-08-19 11:24:26 +0700

    Delete Informatics Task directory

--- Informatics Task/Find Error/kalender/Debug.py
+++
@@ -1,9 +0,0 @@
-# Mengimpor modul calendar
-import calendar
-
-# Menginput tahun dan bulan
-yy = int(input("Masukan Tahun : "))
-mm = int(input("Masukan Bulan : "))
-
-# Menampilkan kalender bulan
-print(calendar.month(yy, mm))
