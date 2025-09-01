ef3dc6fba7944ee568b115a3005aa8e49682912c
Agera-One <182347485+Agera-One@users.noreply.github.com>
2025-09-01 14:30:51 +0700

    Delete Informatics/Find Error directory

--- Informatics/Find Error/kalender/Error.py
+++
@@ -1,9 +0,0 @@
-# Mengimpor modul calendar
-impor calndar # error
-
-# Menginput tahun dan bulan
-yy = int(input("Masukan Tahun : ")) # error
-mm = int(input(Masukan Bulan : )) # error
-
-# Menampilkan kalender bulan
-print(calendarmonth(yy, mm)) # error
